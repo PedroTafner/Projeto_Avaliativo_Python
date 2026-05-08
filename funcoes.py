ce89fc924b2e8ce3b65f18602532a075bf3dccaa
PedroTafner <pedro.htr1@puccampinas.edu.br>
2026-05-08 13:08:50 -0300

    Organização das pastas e arquivos

--- funcoes.py
+++ funcoes.py
@@ -1,47 +1,52 @@
-def mediaTotal(x,y):
-    mediaPressoes = x / y
-    return mediaPressoes
-
 def acharNovo(upcAtual):
-    if upcAtual>150:
-        upcNovo=upcAtual*1.08
+    if upcAtual > 150:
+        upcNovo = upcAtual*1.08
+
     else:
-        upcNovo=upcAtual*0.96
+        upcNovo = upcAtual*0.96
+
     return upcNovo
 
+
 def menor(menor,upcNovo):
-    if menor>upcNovo:
+    if menor > upcNovo:
         menor = upcNovo
+    
     return menor
 
 
 def resultados(mediaPressoes, menor, zVerde,porcentagemDeLeitura ):
-    print('RESULTADOS')
-    print('------------------------')
-    print(f'A media das pressões foi de {mediaPressoes:.2f}')
-    print(f'O menor UPC registrado foi {menor:.2f}')
-    print(f'O total de Zonas Verdes registradas foi de {zVerde}')
-    print(f'A porcentagem de leituras realizadas é de {porcentagemDeLeitura:.2f}%')
-    
+    print("\n-------------------------------------------------------------------------------------------------------")
+    print('\n\tRESULTADO DAS LEITURAS DA REFINARIA:')
+    print(f'\n\tA media das pressões foi de {mediaPressoes:.2f}')
+    print(f'\tO menor UPC registrado foi {menor:.2f}')
+    print(f'\tO total de Zonas Verdes registradas foi de {zVerde}')
+
+    if porcentagemDeLeitura != 100:
+        print(f'\tA porcentagem de leituras realizadas é de {porcentagemDeLeitura:.2f}%')
+        
+    print("\n-------------------------------------------------------------------------------------------------------")
 
 
-def classificacao(upcNovo,zAmarela,zVerde,zVermelha):
-    
-    if upcNovo>120 and upcNovo<180:
-        zVerde+=1
+def classificacao(upcNovo,zAmarela,zVerde,zVermelha):    
+    if upcNovo > 120 and upcNovo < 180:
+        zVerde += 1
         zVermelha = 0   
-    elif upcNovo>180 and upcNovo<250:
-        zAmarela+=1
+    
+    elif upcNovo > 180 and upcNovo < 250:
+        zAmarela += 1
         zVermelha = 0
+    
     else:
-        zVermelha+=1
+        zVermelha += 1
+    
     return zAmarela,zVerde,zVermelha
 
 
 def media(somaPressoes, leituraRealizada):
-   resultado = somaPressoes/leituraRealizada
+   resultado = somaPressoes / leituraRealizada
    return resultado
 
 def porcentagem(leituraRealizada, leitura):
-   resultado =  (leituraRealizada/leitura)*100
+   resultado = (leituraRealizada/leitura) * 100
    return resultado
--- main.py
+++ main.py
@@ -1,45 +1,49 @@
-import funcoes
-print("\nBem vindo ao ") 
+import funcoes as f
+print("\nBem vindo ao programa da Refinaria Delta-9") 
 
-leitura=int(input("Digite o número total de leituras da pressão hidrodinâmina que serão realizadas no seu turno: "))
-zVerde=0
-zAmarela=0
-zVermelha=0
+leitura = int(input("Digite o número total de leituras da pressão hidrodinâmina que serão realizadas no seu turno: "))
+
+zVerde = 0
+zAmarela = 0
+zVermelha = 0
 menor = 400
 leituraRealizada = 0
+somaPressoes = 0
 
-somaPressoes=0
+while leitura <= 0:
+    print("Número de leitura inválida, tente novamente.")
+    leitura = int(input("\nDigite o número total de leituras da pressão hidrodinâmina que serão realizadas no seu turno: "))
 
+print("\n-------------------------------------------------------------------------------------------------------")
 for i in range(leitura):
-    if zVermelha!= 2: 
-
-        upcAtual=float(input("Digite o valor atual da Pressão Hidrodinâmica: "))
-
-        upcNovo=funcoes.acharNovo(upcAtual)
-
-        menor = funcoes.menor(menor, upcNovo)
-        if upcNovo>120:
-            
-
-            somaPressoes+=upcNovo
+    if zVermelha != 2: 
+        upcAtual = float(input(f"\nLeitura N°{i+1}\nDigite o valor da Pressão Hidrodinâmica: "))
         
-            zAmarela, zVerde, zVermelha= funcoes.classificacao(upcNovo,zAmarela,zVerde,zVermelha)
+        upcNovo = f.acharNovo(upcAtual)
+        menor = f.menor(menor, upcNovo)
+
+        if upcNovo > 120:
+            somaPressoes += upcNovo
+            zAmarela, zVerde, zVermelha = f.classificacao(upcNovo,zAmarela,zVerde,zVermelha)
+        
         else:
-            print("O fluido foi cristalizado e entupiu")
-            leituraRealizada +=1
+            print("\n-------------------------------------------------------------------------------------------------------")
+            print("\n\tERRO DETECTADO:\n\tO fluido foi cristalizado devido a baixa pressão constrita e entupiu o duto, interrompendo o escoamento.")
+            
+            leituraRealizada += 1
             break
         
-
-    else:
-        print("\nO escoamento irá ser interrompido imediatamente por segurança")
+    if zVermelha == 2:
+        print("\n-------------------------------------------------------------------------------------------------------")
+        print("\nERRO DETECTADO:\n\tDevido à alta pressão constrita, o escoamento será interrompido imediatamente por segurança.")
         break
-       
-      
+             
+    leituraRealizada += 1
 
-    leituraRealizada +=1
 
-porcentagemDeLeitura = funcoes.porcentagem(leituraRealizada, leitura)
-mediaPressoes = funcoes.media(somaPressoes,leituraRealizada)
+porcentagemDeLeitura = f.porcentagem(leituraRealizada, leitura)
+mediaPressoes = f.media(somaPressoes,leituraRealizada)
+f.resultados(mediaPressoes, menor, zVerde, porcentagemDeLeitura)
 
-funcoes.resultados(mediaPressoes, menor, zVerde,porcentagemDeLeitura)
-  
+input("\nAperte ENTER para finalizar...")
+
